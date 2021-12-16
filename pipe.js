0da7859413865c2ee6cb11f3b7f2b0168a276d4a
Hevin Jant <hevin.jant@gmail.com>
2021-12-16 01:47:55 -0800

    bugs fixed

--- pipe.js
+++ pipe.js
@@ -72,7 +72,7 @@ function createPipe() {
     rects() {
       return [
         topElem.getBoundingClientRect(),
-        bottomElem.getBoundingClientRect,
+        bottomElem.getBoundingClientRect(),
       ];
     },
   };
